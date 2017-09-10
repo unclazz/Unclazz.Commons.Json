b6b4691b62652406aedee0eb31cc91d9062f260b
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-09-10 11:52:16 +0900

    bugfix: NUnitでParserException.Messageが正しくクライアントに表示されない

--- Unclazz.Commons.Json/Parser/ParseException.cs
+++ Unclazz.Commons.Json/Parser/ParseException.cs
@@ -7,36 +7,24 @@ namespace Unclazz.Commons.Json.Parser
 	/// </summary>
 	public sealed class ParseException : Exception
 	{
-        private readonly string message;
         /// <summary>
 		/// パース処理の入力オブジェクト
 		/// </summary>
         public Input Input { get; private set; }
-		/// <summary>
-		/// 例外メッセージ
-		/// </summary>
-        public override string Message
-        {
-            get
-            {
-                return message;
-            }
-        }
         /// <summary>
         /// コンストラクタ.
         /// </summary>
         /// <param name="input">例外発生時の<see cref="Input"/>インスタンス.</param>
-		public ParseException(Input input)
+		public ParseException(Input input) : base(MakeMessage(input, null))
 		{
 			Input = input;
-            message = MakeMessage(input, "error has occurred.");
 		}
         /// <summary>
         /// コンストラクタ.
         /// </summary>
         /// <param name="input">例外発生時の<see cref="Input"/>インスタンス.</param>
         /// <param name="message">メッセージ.</param>
-		public ParseException(Input input, string message)
+		public ParseException(Input input, string message) : base(MakeMessage(input, message))
 		{
 			Input = input;
             message = MakeMessage(input, message);
@@ -47,15 +35,14 @@ namespace Unclazz.Commons.Json.Parser
         /// <param name="input">例外発生時の<see cref="Input"/>インスタンス.</param>
         /// <param name="message">メッセージ.</param>
         /// <param name="cause">原因となった例外.</param>
-		public ParseException(Input input, string message, Exception cause) : base(message, cause)
+		public ParseException(Input input, string message, Exception cause) : base(MakeMessage(input, message), cause)
 		{
-			Input = input;
-            message = MakeMessage(input, message);
+            Input = input;
 		}
-		private string MakeMessage(Input input, string message)
+		static string MakeMessage(Input input, string message)
 		{
 			return string.Format("at line {0}, column {1}. {2}",
-				input.LineNumber, input.ColumnNumber, message);
+				input.LineNumber, input.ColumnNumber, message ?? "unexpected error has occurred.");
 		}
 	}
 }
